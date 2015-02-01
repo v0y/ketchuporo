bfbf2785686c131fe6540018c8d35e7d13ecb165
Rafał Mirończyk <voyageur.pl@gmail.com>
2015-02-02 00:44:50 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+requires = [
+    'kivy==1.8.0',
+]
+
+
+setup(
+    name='Ketchuporo',
+    version='0.1',
+    description='Pomodoro timer',
+    author='Rafał Mirończyk',
+    author_email='voy@lolwtf.pl',
+    url='https://github.com/v0y/ketchuporo',
+    license='MIT',
+    install_requires=requires,
+    packages=['ketchuporo'],
+)
